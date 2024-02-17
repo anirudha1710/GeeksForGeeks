53d0593b6a5a30f93cc378cbbfd4eeb4d6ebac87
Anirudha Sharma <114614344+anirudha1710@users.noreply.github.com>
2024-02-17 17:35:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Frogs and Jumps/frogs-and-jumps.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            
+            String inputLine1[] = br.readLine().trim().split(" "); 
+            int i=0;
+            int N = Integer.parseInt(inputLine1[i++]);
+            int leaves = Integer.parseInt(inputLine1[i++]);
+            
+            int frogs[] = new int[(int)(N)];
+            
+            String inputLine2[] = br.readLine().trim().split(" ");
+            for ( i = 0; i < N; i++) {
+                frogs[i] = Integer.parseInt(inputLine2[i]);
+            }
+        
+            Solution ob = new Solution();
+            System.out.println(ob.unvisitedLeaves(N, leaves, frogs));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        // Code here
+        int[] vis = new int[leaves+1];
+        for(int i=0;i<N;i++){
+            int val = frogs[i];
+            if(val <= leaves && vis[val]==0){
+                int j = val;
+               while(j<=leaves){
+                    vis[j] = 1;
+                    j+=val;
+               }
+            }
+        }
+        int count =0;
+        for(int i =1;i<vis.length;i++){
+            if(vis[i] == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}

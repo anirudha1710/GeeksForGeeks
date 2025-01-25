f24c10b722b70e5ae68e9b83a0b9411570093c8c
Anirudha Sharma <114614344+anirudha1710@users.noreply.github.com>
2025-01-25 23:43:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+
+            String[] line1 = br.readLine().trim().split(" ");
+            int[] a = new int[line1.length];
+            for (int i = 0; i < line1.length; i++) {
+                a[i] = Integer.parseInt(line1[i]);
+            }
+
+            String[] line2 = br.readLine().trim().split(" ");
+            int[] b = new int[line2.length];
+            for (int i = 0; i < line2.length; i++) {
+                b[i] = Integer.parseInt(line2[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.kthElement(a, b, k));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        int arr[] = new int[a.length+b.length];
+        System.arraycopy(a,0,arr,0,a.length);
+        System.arraycopy(b,0,arr,a.length,b.length);
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+        if(i==k-1){
+            return arr[i];
+        }
+        }
+        return 0;
+    }
+}
